7b23e49fd10ada25b758db773aa82a10be629de4
Mahdi Farmahini Farahani <aradfarahani@aol.com>
2025-02-27 23:31:30 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url="https://github.com/FEZtool-team/FEZrs",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
