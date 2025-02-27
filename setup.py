a7208a3af9bf9d889615f00f4c09e915724533b1
Mahdi Farmahini Farahani <aradfarahani@aol.com>
2025-02-28 00:20:35 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fezrs",
-    version="0.2.6",
+    version="0.2.10",
     packages=find_packages(),
     install_requires=[
         "numpy",
