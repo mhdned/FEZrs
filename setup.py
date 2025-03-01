19d2b6b05f33361f33d7ce77a718544047033263
Mahdi Farmahini Farahani <aradfarahani@aol.com>
2025-03-01 22:10:31 +0330

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,35 +1,28 @@
 from setuptools import setup, find_packages
-import setuptools_scm
 
 setup(
     name="fezrs",
-    use_scm_version=True,  # Automatically use version from `VERSION` file or SCM
-    setup_requires=["setuptools", "setuptools_scm"],
-    packages=find_packages(),  # Automatically find and include packages
+    version="0.2.2",
+    packages=find_packages(),
     install_requires=[
         "numpy",
         "matplotlib",
         "scikit-image",
         "scikit-learn",
         "fastapi",
-        "opencv-python",  # Ensuring compatibility with OpenCV's PyPI package
+        "opencv-python" 
+        
     ],
-    include_package_data=True,  # Include non-code files from MANIFEST.in
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A brief description of the Fezrs package",
-    long_description=open("README.md").read(),  # Read from README.md for PyPI
+    author="Mahdi Farmahinifarahani, Hooman Mirzaee, Mahdi Nedaee, Mohammad Hossein Kiani Fayz Abadi, Yoones Kiani Feyz Abadi, Erfan Karimzadehasl, Parsa Elmi",
+    author_email="aradfarahani@aol.com",
+    description="Feature Extraction and Zoning for Remote Sensing (FEZrs)",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/fezrs",  # Replace with your repo URL
+    url="https://github.com/FEZtool-team/FEZrs",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # Adjust if necessary
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
-    entry_points={
-        "console_scripts": [
-            "fezrs=fezrs.__main__:main",  # Replace with actual entry point
-        ],
-    },
 )
