8f121a8931091975a18587aa59d3056e0e3d5030
mhdned <mahdi.nedaee4@gmail.com>
2025-04-03 03:27:20 +0330

    ADD typehints to hsv_calculator ADD title as argument for export_file method REMOVE run method and put that inside BaseTool (class parent)

--- fezrs/tools/hsv/hsv_calculator.py
+++ fezrs/tools/hsv/hsv_calculator.py
@@ -1,17 +1,13 @@
 # Import packages and libraries
 import numpy as np
 from uuid import uuid4
-from typing import Union
 from pathlib import Path
-import matplotlib.cm as cm
 from skimage.color import rgb2hsv
 from matplotlib import pyplot as plt
 
 # Import module and files
 from fezrs.base import BaseTool
-
-# HSVCalculator class
-PathLike = Union[str, Path]
+from fezrs.utils.type_handler import PathLike
 
 # Calculator class
 class HSVCalculator(BaseTool):
@@ -25,16 +21,16 @@ class HSVCalculator(BaseTool):
         ## __init__ (method)
         Initializes the HSVCalculator with paths to Near-Infrared (NIR), Blue, and Green band images.
         
-        ## Description:
+        ### Description:
         This class processes multispectral remote sensing images by normalizing the input bands
         and converting them into the HSV (Hue, Saturation, Value) color space.
 
-        Args:
+        ### Args:
             nir_path (str or Path): File path to the Near-Infrared (NIR) band image.
             blue_path (str or Path): File path to the Blue band image.
             green_path (str or Path): File path to the Green band image.
 
-        Attributes:
+        ### Attributes:
             normalized_bands (dict): A dictionary containing normalized versions of the NIR, Blue, and Green bands.
             __hsv_output (numpy.ndarray or None): Stores the HSV image after processing. Initially set to None.
         """
@@ -58,7 +54,7 @@ class HSVCalculator(BaseTool):
         ## calculate (method)
         Computes the HSV representation of the input multispectral image.
 
-        Returns:
+        ### Returns:
             np.ndarray: The computed HSV image.
         """
         
@@ -71,6 +67,7 @@ class HSVCalculator(BaseTool):
 
     def export_file(self, 
             output_path: PathLike,
+            title: str | None = None,
             figsize: tuple = (10, 5), 
             show_axis: bool = True, 
             colormap: str = None, 
@@ -82,14 +79,14 @@ class HSVCalculator(BaseTool):
         ## export_file (method)
         Exports the HSV image as a PNG file with customizable visualization options.
 
-        Args:
+        ### Args:
             output_path (str or Path): Directory where the file will be saved.
             figsize (tuple, optional): Figure size in inches. Defaults to (10, 5).
             show_axis (bool, optional): Whether to display axes. Defaults to True.
             colormap (str, optional): Colormap to apply to the image. Defaults to None.
             show_colorbar (bool, optional): Whether to include a colorbar. Defaults to True.
             filename_prefix (str, optional): Prefix for the output filename. Defaults to "HSV_output".
-            dpi (int, optional): Resolution of the saved image. Defaults to 300.
+            dpi (int, optional): Resolution of the saved image. Defaults to 500.
         """
         
         #NOTE - Check if HSV is processed and store in output property 
@@ -112,6 +109,9 @@ class HSVCalculator(BaseTool):
         
         if show_colorbar:
             fig.colorbar(im, ax=ax)
+            
+        if title:
+            plt.title(f"{title}-FEZrs")
 
         filename = f"{output_path}/{filename_prefix}_{uuid4().hex}.png"
         fig.savefig(filename, dpi=dpi, bbox_inches='tight')
@@ -119,20 +119,7 @@ class HSVCalculator(BaseTool):
         
         #FIX Should return the location or output file
         return filename
-    
-    
-    def run(self, output_path: PathLike, **export_kwargs):
-        """
-        ## run (Method)
-        Runs the full pipeline: validation, calculation, and exporting the HSV image.
 
-        Args:
-            output_path (str or Path): Directory where the file will be saved.
-            **export_kwargs: Additional arguments to customize the export (e.g., figsize, colormap).
-        """
-        self.validate()
-        self.calculate()
-        self.export_file(output_path, **export_kwargs)
 
 #NOTE - These block code for test the tools, delete before publish product
 if __name__ == "__main__":
@@ -141,5 +128,4 @@ if __name__ == "__main__":
     green_path = Path.cwd() / "data/Green.tif"
     
     calculator = HSVCalculator(blue_path=blue_path, green_path=green_path, nir_path=nir_path)
-    calculator.run(output_path="./")
     
